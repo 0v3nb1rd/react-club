d271dcadf02bb7a4f6e0a676033e675a25607a39
0v3nb1rd <0v3nb1rd@gmail.com>
2021-11-10 22:37:36 +0200

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Nav } from './Nav';
+import logo from '../assets/img/logo.svg';
 
 export const Header = () => {
   return (
-    <header className="navbar mb-2 shadow-lg bg-neutral text-neutral-content ">
-      <div className="px-2 mx-2 navbar-start">
-        <span className="text-lg font-bold">daisyUI</span>
+    <header className="navbar mb-2 shadow-lg bg-neutral text-neutral-content py-3">
+      <div className="container mx-auto">
+        <a href="./" className="logo px-2 flex items-center absolute">
+          <img className="mr-2" src={logo} alt="" />
+          <span className="text-lg font-bold uppercase font-mono tracking-widest leading-4">
+            React <br /> Club
+          </span>
+        </a>
+        <Nav />
       </div>
-      <Nav />
     </header>
   );
 };
