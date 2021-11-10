836291200beeec1863f7c5148bbbec3dc49c1ab2
0v3nb1rd <0v3nb1rd@gmail.com>
2021-11-10 02:41:17 +0200

    add homepage structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Footer } from './Footer/Footer';
+import { Header } from './Header/Header';
+import { Hero } from './Hero/Hero';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main className="App">
+      <Header />
+      <Hero />
 
-export default App;
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="card text-center ">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://picsum.photos/id/1005/400/250"
+              className="rounded-xl"
+              alt="images"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">shadow, center, padding</h2>
+            <p>
+              Rerum reiciendis beatae tenetur excepturi aut pariatur est eos.
+              Sit sit necessitatibus veritatis sed molestiae voluptates incidunt
+              iure sapiente.
+            </p>
+          </div>
+        </div>
+        <div className="card text-center ">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://picsum.photos/id/1005/400/250"
+              className="rounded-xl"
+              alt="images"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">shadow, center, padding</h2>
+            <p>
+              Rerum reiciendis beatae tenetur excepturi aut pariatur est eos.
+              Sit sit necessitatibus veritatis sed molestiae voluptates incidunt
+              iure sapiente.
+            </p>
+          </div>
+        </div>
+        <div className="card text-center ">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://picsum.photos/id/1005/400/250"
+              className="rounded-xl"
+              alt="images"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">shadow, center, padding</h2>
+            <p>
+              Rerum reiciendis beatae tenetur excepturi aut pariatur est eos.
+              Sit sit necessitatibus veritatis sed molestiae voluptates incidunt
+              iure sapiente.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
+};
