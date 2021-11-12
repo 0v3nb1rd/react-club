3c36fb250fcfb2dfa953fc5ec8d25c992163f6c3
0v3nb1rd <0v3nb1rd@gmail.com>
2021-11-12 21:33:48 +0200

    improve structure for routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import { Header, Footer, Hero, Post, Button } from './Components';
+import { Home } from './Pages/Home';
 import { posts } from './store';
+
 export const App = () => {
   return (
     <>
       <Header />
       <main>
-        <Hero />
-        <section className="container mx-auto -mt-20 mb-20">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {posts.map((post, idx) => (
-              <Post key={idx} post={post} />
-            ))}
-          </div>
-
-          <Button className="btn-accent btn-outline btn-wide btn-md flex mx-auto mt-10">
-            View more
-          </Button>
-        </section>
+        <Home posts={posts} />
       </main>
       <Footer />
     </>
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Header, Footer, Hero, Post, Button } from '../Components';
+
+export const Home = ({ posts }) => {
+  return (
+    <>
+      <Hero />
+      <section className="container mx-auto -mt-20 mb-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {posts.map((post, idx) => (
+            <Post key={idx} post={post} />
+          ))}
+        </div>
+
+        <Button className="btn-accent btn-outline btn-wide btn-md flex mx-auto mt-10">
+          View more
+        </Button>
+      </section>
+    </>
+  );
+};
