c39d3c09ed7874750509bc1e0b1aae81cc8588e5
0v3nb1rd <0v3nb1rd@gmail.com>
2021-11-14 15:31:15 +0200

    add Search component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Nav, Logo } from '.';
+import { Search } from './Search';
 
 export const Header = () => {
   return (
     <header className="navbar  shadow-lg bg-neutral text-neutral-content py-4">
-      <div className="container  mx-auto">
+      <div className="container  mx-auto relative">
         <Logo className="absolute" />
         <Nav />
+        <Search />
       </div>
     </header>
   );
