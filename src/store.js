fd60d1db1bcbf7980a7687cb866d3188b30c197b
0v3nb1rd <0v3nb1rd@gmail.com>
2021-11-14 12:00:48 +0200

    update store

--- src/store.js
+++ src/store.js
@@ -32,15 +32,15 @@ export const products = [
   {
     id: '1',
     title: 'Iphone 13pro',
-    desc:
-      "Introduced on September 14, the iPhone 13 and iPhone 13 mini are Apple's newest flagship iPhones on the more affordable end, and are being sold alongside the more expensive iPhone 13 Pro and iPhone 13 Pro Max. The iPhone 13 and iPhone 13 mini are ideal for those who don't need pro-level camera features...",
+    shortText:
+      "Introduced on September 14, the iPhone 13 and iPhone 13 mini are Apple's newest flagship iPhones on the more affordable end, and are being sold alongside the more expensive iPhone 13 Pro and iPhone 13 Pro Max...",
     img: productPhone,
     price: '999',
   },
   {
     id: '2',
-    title: 'MacBooc M1Max',
-    desc:
+    title: 'MacBook Max',
+    shortText:
       "Apple is continuing it's crusade to banish Intel from its lineup with two more processors: the M1 Pro and M1 Max, professional-grade processors debuting in the 14-inch and 16-inch MacBook Pros...",
     img: productMac,
     price: '2499',
@@ -48,15 +48,15 @@ export const products = [
   {
     id: '3',
     title: 'AOC Monitor 4K',
-    desc:
-      'With decades of experience, our technological expertise forms the basis of AOC’s best quality led &amp; lcd monitors. Our engineers incorporate the latest innovations into AOC’s computer monitors, keeping our customers at the forefront of all technological developments. Year after year, trade publications have awarded AOC for their performance in manufacturing best quality led and lcd monitors...',
+    shortText:
+      'With decades of experience, our technological expertise forms the basis of AOC’s best quality led &amp; lcd monitors. Our engineers incorporate the latest innovations into AOC’s computer monitors...',
     img: productMonitor,
     price: '1099',
   },
   {
     id: '4',
     title: 'Amazon Kindle',
-    desc:
+    shortText:
       'Lucky for us, we can use special snippets of code in our Amazon listings to access their font styles. All you need to do is type the right things around the sentences in your product description to make the words stand out and look great...',
     img: productKindle,
     price: '199',
