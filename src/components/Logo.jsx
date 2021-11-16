bdae7442a1b77252157b51c6af69fe8d4a1b0744
0v3nb1rd <0v3nb1rd@gmail.com>
2021-11-16 23:07:50 +0200

    add logo on click

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import logoImg from '../assets/img/logo.svg';
 import cn from 'classnames';
+import { Link } from 'react-router-dom';
 
 export const Logo = (props) => {
   const { className } = props;
 
   return (
-    <a href="./" className={cn('logo px-2 flex items-center', className)}>
+    <Link to="/" className={cn('logo px-2 flex items-center', className)}>
       <img className="mr-2" src={logoImg} alt="logo " />
       <span className="text-lg font-bold uppercase font-mono tracking-widest leading-4">
         React <br /> Club
       </span>
-    </a>
+    </Link>
   );
 };
