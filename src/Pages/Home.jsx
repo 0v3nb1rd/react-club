2d045a7e9dc7d97a3241e6a136264c1d83520e3c
0v3nb1rd <0v3nb1rd@gmail.com>
2021-11-17 19:33:19 +0200

    fix show more functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Hero, Post, Button } from '../components';
 
 export const Home = ({ posts }) => {
+  const ITEMS_ON_PAGE = 3;
   const [items, setItems] = React.useState(posts);
-  const [visible, setVisible] = React.useState(3);
+  const [visible, setVisible] = React.useState(ITEMS_ON_PAGE);
   const showMoreItems = () => {
-    setVisible((prev) => prev * 2);
+    setVisible((prev) => prev + ITEMS_ON_PAGE);
   };
-  console.log(items);
+  const hasPost = () => visible < items.length;
+
   return (
     <>
       <Hero />
@@ -18,13 +20,14 @@ export const Home = ({ posts }) => {
               .slice(0, visible)
               .map((post, idx) => <Post key={idx} post={post} />)}
         </div>
-
-        <Button
-          onClick={showMoreItems}
-          className="btn-accent btn-outline btn-wide btn-md flex mx-auto mt-10"
-        >
-          View more
-        </Button>
+        {hasPost() && (
+          <Button
+            onClick={showMoreItems}
+            className="btn-accent btn-outline btn-wide btn-md flex mx-auto mt-10"
+          >
+            View more
+          </Button>
+        )}
       </section>
     </>
   );
